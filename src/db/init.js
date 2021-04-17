84762de9ca8914dae1b3716009f9cc22bbe688dc
Leonardo Klestadt Luz <klestadt@hotmail.com>
2021-04-17 19:53:57 -0300

    initial commit

---
+++ src/db/init.js
@@ -0,0 +1,89 @@
+// importando o arquivo config do banco de dados
+const Database = require("./config");
+
+const initDb = {
+  async init() {
+    // iniciando a conexão com o banco de dados
+    const db = await Database();
+
+    // criação da tabela profile
+    await db.exec(`
+            CREATE TABLE profile (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT,
+                avatar TEXT,
+                monthly_budget INT,
+                days_per_week INT,
+                hours_per_day INT,
+                vacation_per_year INT,
+                value_hour INT
+            );
+        `);
+
+    // criação da tabela jobs
+    await db.exec(`
+            CREATE TABLE jobs (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT,
+                daily_hours INT,
+                total_hours INT,
+                created_at DATETIME
+            );
+        `);
+
+    // inserindo dados nas tabelas
+    await db.run(`
+            INSERT INTO profile(
+                name,
+                avatar,
+                monthly_budget,
+                days_per_week,
+                hours_per_day,
+                vacation_per_year,
+                value_hour 
+            ) VALUES (
+                "Leonardo", 
+                "https://avatars.githubusercontent.com/u/19575296?v=4", 
+                3000, 
+                5, 
+                5,
+                4,
+                75
+            );
+        `);
+
+    await db.run(`
+            INSERT INTO jobs(
+                name,
+                daily_hours,
+                total_hours,
+                created_at 
+            ) VALUES (
+                "Pizzaria Guloso", 
+                2,
+                1,
+                1617514376018
+            )
+        `);
+
+    await db.run(`
+            INSERT INTO jobs(
+                name,
+                daily_hours,
+                total_hours,
+                created_at 
+            ) VALUES (
+                "OneTwo Project", 
+                3,
+                47,
+                1617514376018
+            );
+        `);
+
+    // fechando a conexão com o banco de dados
+    await db.close();
+  },
+};
+
+// chamada da função init()
+initDb.init();
